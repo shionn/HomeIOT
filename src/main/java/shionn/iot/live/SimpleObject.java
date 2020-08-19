f85309cb26aeade74ed411012568604fd06b5daa
Shionn <shionn@gmail.com>
2020-08-19 07:50:20 +0200

    start

---
+++ src/main/java/shionn/iot/live/SimpleObject.java
@@ -0,0 +1,15 @@
+package shionn.iot.live;
+
+public class SimpleObject {
+
+	private String message;
+
+	public SimpleObject(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
