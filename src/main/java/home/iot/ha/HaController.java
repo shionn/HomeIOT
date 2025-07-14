8e077082e5941201345dcb212971f09ae80d8734
Kepka Ludovic <shionn@gmail.com>
2025-07-14 09:51:10 +0200

    Ajout passeral HA

--- src/main/java/home/iot/ha/HaController.java
+++ src/main/java/home/iot/ha/HaController.java
@@ -1,10 +1,12 @@
 package home.iot.ha;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HaController {
@@ -14,13 +16,14 @@ public class HaController {
 
 	@GetMapping("/ha")
 	@ResponseBody
-//	@ResponseStatus(value = HttpStatus.ACCEPTED)
+	@ResponseStatus(value = HttpStatus.ACCEPTED)
 	public String getAll() {
 		return service.getAllData();
 	}
 
 	@GetMapping("/ha/{id}")
 	@ResponseBody
+	@ResponseStatus(value = HttpStatus.ACCEPTED)
 	public String getLastValue(@PathVariable("id") String id) {
 		return service.getSensorData(id).getState();
 	}
