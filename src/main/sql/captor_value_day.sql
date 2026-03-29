4104bd9e6ca6b20dd024d7796dc85b10a87379dc
Kepka Ludovic <shionn@gmail.com>
2026-03-29 17:08:34 +0200

    changement de serveur

--- src/main/sql/captor_value_day.sql
+++ src/main/sql/captor_value_day.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE VIEW captor_value_day AS
-SELECT cv.captor, c.name, CONVERT ('max' USING utf8mb4) AS type, MAX(cv.value) AS value, DATE(cv.date) AS date_c 
+SELECT cv.captor, c.name, CONVERT ('max' USING utf8mb4) AS type, MAX(cv.value) AS value, DATE(cv.date) AS date_c
 	FROM captor_value AS cv
 	LEFT JOIN captor AS c ON c.id = cv.captor
 	GROUP BY date_c, captor
 UNION
-SELECT cv.captor, c.name, CONVERT ('min' USING utf8mb4) AS type, MIN(cv.value) AS value, DATE(cv.date) AS date_c 
+SELECT cv.captor, c.name, CONVERT ('min' USING utf8mb4) AS type, MIN(cv.value) AS value, DATE(cv.date) AS date_c
 	FROM captor_value AS cv
 	LEFT JOIN captor AS c ON c.id = cv.captor
-	GROUP BY date_c, captor;<
+	GROUP BY date_c, captor;
